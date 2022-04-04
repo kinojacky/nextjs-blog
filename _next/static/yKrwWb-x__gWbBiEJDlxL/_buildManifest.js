90733cc11a255aa0f1112a1aa750a613c4450703
kinojacky <kinojacky@icloud.com>
2022-04-04 06:17:31 +0000

    Deploying to gh-pages from @ kinojacky/nextjs-blog@654fe69b310348febc3cd2e58dd817e957a5ebc2 🚀

--- _next/static/yKrwWb-x__gWbBiEJDlxL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-c7b8ab2072535f4f2970.js"],"/_error":["static/chunks/pages/_error-d799e3a96772416696b7.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
